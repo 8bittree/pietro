44376cf3f47db5ec40b2da0821c09e858a54bbe3
Jon Olin <github.jao@jon.tomolin.net>
2017-09-06 23:44:09 -0400

    List files in /Applications

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::{self, DirEntry};
+use std::path::{Path, PathBuf};
+
 fn main() {
     println!("Hello, world!");
+    let files = list_files("/Applications");
+    println!("{:?}", files);
+}
+
+fn list_files<P: AsRef<Path>>(dir: P) -> Vec<PathBuf> {
+    let mut files = Vec::new();
+    if dir.as_ref().is_dir() {
+        if let Ok(mut dirEntries) = fs::read_dir(dir) {
+            while let Some(Ok(entry)) = dirEntries.next() {
+                files.push(entry.path());
+            }
+        }
+    }
+    files
 }
